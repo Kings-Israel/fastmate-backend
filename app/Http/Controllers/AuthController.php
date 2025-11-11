5fb20be1350b4d178ab04e85fe47ed3e9459d063
Kings-Israel <milimokings@gmail.com>
2025-11-11 19:58:15 +0300

    Update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        info($request->all());
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
